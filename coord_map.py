821f4acbc94a3458bf74c816957a49ced45d1e72
Leslie Lamarche <leslie.lamarche@sri.com>
2019-08-23 09:08:57 -0700

    Initial proof of concept.

---
+++ coord_map.py
@@ -0,0 +1,101 @@
+# coord_map.py
+
+import numpy as np
+from apexpy import Apex
+import pymap3d as pm
+import marp as M
+
+import matplotlib.pyplot as plt
+import matplotlib.gridspec as gridspec
+import cartopy.crs as ccrs
+
+def Richmond1995_fig3():
+    A = Apex(1995)
+
+    lat, lon = np.meshgrid(np.arange(-90.,90.,5.),np.arange(-180.,180.,5.))
+
+    f1, f2, f3, g1, g2, g3, d1, d2, d3, e1, e2, e3 = A.basevectors_apex(lat.ravel(), lon.ravel(), 110., coords='geo')
+
+    d1xd2 = np.cross(d1.T, d2.T).T
+    D = np.linalg.norm(d1xd2, axis=0).reshape(lat.shape)
+
+    d12 = np.einsum('i...,i...->...', d1, d1).reshape(lat.shape)/D
+    d1d2 = np.einsum('i...,i...->...', d1, d2).reshape(lat.shape)/D
+    d22 = np.einsum('i...,i...->...', d2, d2).reshape(lat.shape)/D
+
+    fig = plt.figure(figsize=(15,10))
+    gs = gridspec.GridSpec(2,2)
+
+    for i, val in enumerate([D,d12,d1d2,d22]):
+        ax = plt.subplot(gs[i], projection=ccrs.PlateCarree())
+        ax.coastlines()
+        c = ax.contourf(lon, lat, val, 30, cmap=plt.get_cmap('jet'), transform=ccrs.PlateCarree())
+
+        plt.colorbar(c)
+    plt.show()
+
+
+def coord_map():
+
+    lat, lon = np.meshgrid(np.arange(-90.,91.,5.),np.arange(0.,361.,5.))
+    lat = lat.flatten()
+    lon = lon.flatten()
+
+
+    A = Apex(2019)
+    f1, f2, f3, g1, g2, g3, d1, d2, d3, e1, e2, e3 = A.basevectors_apex(lat, lon, 0., coords='apex')
+    d1xd2 = np.cross(d1.T, d2.T).T
+    D = np.linalg.norm(d1xd2, axis=0)
+    d12 = np.einsum('i...,i...->...', d1, d1)/D
+    d1d2 = np.einsum('i...,i...->...', d1, d2)/D
+    d22 = np.einsum('i...,i...->...', d2, d2)/D
+    apex = [lat, D, d12, d1d2, d22]
+    labels = ['MLAT', r'$D$', r'$d_1^2/D$', r'$d_1\cdot d_2/D$', r'$d_2^2/D$']
+
+
+    marp = []
+    marp_lat = []
+    marp_lon = []
+    marp_title = []
+    for (lat0, lon0) in [(0.,60.), (30.,60.), (60.,60.), (90.,60.)]:
+        rplat, rplon = M.ma2marp(lat, lon, lat0, lon0)
+        marp_d1, marp_d2, marp_d3 = M.base_vectors(lat, lon, lat0, lon0)
+        marp_d1xd2 = np.cross(marp_d1.T, marp_d2.T).T
+        marp_D = np.linalg.norm(marp_d1xd2, axis=0)
+        marp_d12 = np.einsum('i...,i...->...', marp_d1, marp_d1)/marp_D
+        marp_d1d2 = np.einsum('i...,i...->...', marp_d1, marp_d2)/marp_D
+        marp_d22 = np.einsum('i...,i...->...', marp_d2, marp_d2)/marp_D
+        marp.append([lat, marp_D, marp_d12, marp_d1d2, marp_d22])
+        marp_lat.append(rplat)
+        marp_lon.append(rplon)
+        marp_title.append('({}, {})'.format(lat0,lon0))
+
+
+    fig = plt.figure(figsize=(15,10))
+    gs = gridspec.GridSpec(len(apex), len(marp)+1)
+    gs.update(left=0.05,right=0.92,top=0.99,bottom=0.01,hspace=0.01,wspace=0.1)
+
+    for i in range(len(apex)):
+        ax = plt.subplot(gs[i,0], projection=ccrs.Mollweide())
+        c = ax.scatter(lon, lat, c=apex[i], cmap=plt.get_cmap('jet'), s=2.5, transform=ccrs.PlateCarree())
+        ax.set_title('Apex')
+        ax.text(-0.25, 0.5, labels[i], transform=ax.transAxes)
+
+        for j in range(len(marp)):
+            ax = plt.subplot(gs[i,j+1], projection=ccrs.Mollweide())
+            ax.scatter(marp_lon[j], marp_lat[j], c=marp[j][i], vmin=min(apex[i]), vmax=max(apex[i]), cmap=plt.get_cmap('jet'), s=2.5, transform=ccrs.PlateCarree())
+            ax.set_title(marp_title[j])
+
+        axpos = ax.get_position() # get the original position 
+        cax = fig.add_axes([axpos.x1 + 0.01, axpos.y0,  0.02, axpos.y1-axpos.y0])
+        plt.colorbar(c, cax=cax)
+
+    plt.savefig('coord_map.png')
+    plt.show()
+
+def main():
+    # Richmond1995_fig3()
+    coord_map()
+
+if __name__=='__main__':
+    main()
---
+++ marp.py
@@ -0,0 +1,155 @@
+# marp.py
+
+import numpy as np
+from apexpy import Apex
+import pymap3d as pm
+
+RE = 6371.
+hR = 0.
+
+def ma2marp(lam, phi, lam0, phi0):
+
+    lam = lam*np.pi/180.
+    phi = phi*np.pi/180.
+    lam0 = lam0*np.pi/180.
+    phi0 = phi0*np.pi/180.
+
+    # xr = np.cos(lam0)*np.cos(lam)*np.cos(phi0-phi) + np.sin(lam0)*np.sin(lam)
+    # yr = -np.cos(lam)*np.sin(phi0-phi)
+    # zr = -np.sin(lam0)*np.cos(lam)*np.cos(phi0-phi) + np.cos(lam0)*np.sin(lam)
+
+    xr = np.cos(phi0)*np.cos(lam0)*np.cos(lam)*np.cos(phi) + np.cos(phi0)*np.sin(lam0)*np.sin(lam) + np.sin(phi0)*np.cos(lam)*np.sin(phi)
+    yr = -np.sin(phi0)*np.cos(lam0)*np.cos(lam)*np.cos(phi) - np.sin(phi0)*np.sin(lam0)*np.sin(lam) + np.cos(phi0)*np.cos(lam)*np.sin(phi)
+    zr = -np.sin(lam0)*np.cos(lam)*np.cos(phi) + np.cos(lam0)*np.sin(lam)
+
+    phir = np.arctan2(yr, xr)
+    lamr = np.arcsin(zr)
+
+    # lamr = np.arcsin(np.cos(lam0)*np.sin(lam) - np.cos(phi)*np.sin(lam0)*np.cos(lam))
+    # phir = np.arctan2(np.sin(phi), np.tan(lam)*np.sin(lam0) + np.cos(phi)*np.cos(lam0)) - phi0
+
+    return lamr*180./np.pi, phir*180./np.pi
+
+def base_vectors(lam, phi, lam0, phi0):
+    Apx = Apex(2019)
+    f1, f2, f3, g1, g2, g3, d1, d2, d3, e1, e2, e3 = Apx.basevectors_apex(lam, phi, 0., coords='apex')
+    glat, glon, _ = Apx.apex2geo(lam, phi, 0.)
+    u, v, w = pm.enu2uvw(d1[0], d1[1], d1[2], glat, glon)
+    d1 = np.array([u, v, w])
+    u, v, w = pm.enu2uvw(d2[0], d2[1], d2[2], glat, glon)
+    d2 = np.array([u, v, w])
+
+    lamr, phir = ma2marp(lam, phi, lam0, phi0)
+
+    lam = lam*np.pi/180.
+    phi = phi*np.pi/180.
+    lam0 = lam0*np.pi/180.
+    phi0 = phi0*np.pi/180.
+    lamr = lamr*np.pi/180.
+    phir = phir*np.pi/180.
+
+    # xr = np.cos(phi0)*np.cos(lam0)*np.cos(lam)*np.cos(phi) + np.cos(phi0)*np.sin(lam0)*np.sin(lam) + np.sin(phi0)*np.cos(lam)*np.sin(phi)
+    # yr = -np.sin(phi0)*np.cos(lam0)*np.cos(lam)*np.cos(phi) - np.sin(phi0)*np.sin(lam0)*np.sin(lam) + np.cos(phi0)*np.cos(lam)*np.sin(phi)
+    # zr = -np.sin(lam0)*np.cos(lam)*np.cos(phi) + np.cos(lam0)*np.sin(lam)
+
+    # phir = np.arctan2(yr, xr)
+    # lamr = np.arcsin(zr)
+
+    d1r = 1/np.cos(lamr)*((np.sin(phir+phi0)*np.cos(lam0)*np.cos(lam)*np.sin(phi)+np.cos(phir+phi0)*np.cos(lam)*np.cos(phi))*d1 + (np.sin(phir+phi0)*np.cos(lam0)*np.sin(lam)*np.cos(phi)-np.sin(phir+phi0)*np.sin(lam0)*np.cos(lam)-np.cos(phir+phi0)*np.sin(lam)*np.sin(phi))*d2)
+    d2r = 1/np.cos(lamr)*(np.sin(lam0)*np.cos(lam)*np.sin(phi)*d1 + (np.sin(lam0)*np.sin(lam)*np.cos(phi)+np.cos(lam0)*np.cos(lam))*d2)
+
+    d1xd2 = np.cross(d1r.T, d2r.T).T
+    D = np.linalg.norm(d1xd2, axis=0)
+    d3r = d1xd2/D**2
+
+    # Currently, this returns ECEF components
+
+    return d1r, d2r, d3r
+
+# def base_vectors2(lam, phi, lam0, phi0):
+#     Apx = Apex(2019)
+#     f1, f2, f3, g1, g2, g3, d1, d2, d3, e1, e2, e3 = Apx.basevectors_apex(lam, phi, 0., coords='apex')
+#     glat, glon, _ = Apx.apex2geo(lam, phi, 0.)
+#     u, v, w = pm.enu2uvw(d1[0], d1[1], d1[2], glat, glon)
+#     d1 = np.array([u, v, w])
+#     u, v, w = pm.enu2uvw(d2[0], d2[1], d2[2], glat, glon)
+#     d2 = np.array([u, v, w])
+
+#     lam = lam*np.pi/180.
+#     phi = phi*np.pi/180.
+#     lam0 = lam0*np.pi/180.
+#     phi0 = phi0*np.pi/180.
+
+#     xr = np.cos(phi0)*np.cos(lam0)*np.cos(lam)*np.cos(phi) + np.cos(phi0)*np.sin(lam0)*np.sin(lam) + np.sin(phi0)*np.cos(lam)*np.sin(phi)
+#     yr = -np.sin(phi0)*np.cos(lam0)*np.cos(lam)*np.cos(phi) - np.sin(phi0)*np.sin(lam0)*np.sin(lam) + np.cos(phi0)*np.cos(lam)*np.sin(phi)
+#     zr = -np.sin(lam0)*np.cos(lam)*np.cos(phi) + np.cos(lam0)*np.sin(lam)
+
+#     phir = np.arctan2(yr, xr)
+#     lamr = np.arcsin(zr)
+
+#     A = np.sqrt(4-3*np.cos(lam)**2)/2.
+#     # d1r = 1./((RE+hR)*np.cos(lamr))*(np.sin(phir+phi0)*np.cos(lam0)*(np.sin(phi)*d1-A*np.cos(phi)*d2)+np.cos(phir+phi0)*(np.cos(phi)*d1+A*np.sin(phi0)*d2)+np.sin(phir+phi0)*np.sin(lam0)*A/np.tan(lam)*d2)
+#     # d2r = 1./((RE+hR)*np.cos(lamr))*(np.sin(lam0)*(np.sin(phi)*d1-A*np.cos(phi)*d2)-np.cos(lam0)*A/np.tan(lam)*d2)
+#     d1r = (np.sin(phir+phi0)*np.cos(lam0)*(np.sin(phi)*d1-A*np.cos(phi)*d2)+np.cos(phir+phi0)*(np.cos(phi)*d1+A*np.sin(phi0)*d2)+np.sin(phir+phi0)*np.sin(lam0)*A/np.tan(lam)*d2)
+#     d2r = -2*np.sin(lamr)/(np.sqrt(4-3*np.cos(lamr)**2)*np.cos(lamr))*(np.sin(lam0)*(np.sin(phi)*d1-A*np.cos(phi)*d2)-np.cos(lam0)*A/np.tan(lam)*d2)
+
+#     return d1r, d2r
+
+# def base_vectors(lam, phi, lam0, phi0):
+
+#     A = Apex(2019)
+
+#     lam = lam*np.pi/180.
+#     phi = phi*np.pi/180.
+#     lam0 = lam0*np.pi/180.
+#     phi0 = phi0*np.pi/180.
+
+#     xr = np.cos(phi0)*np.cos(lam0)*np.cos(lam)*np.cos(phi) + np.cos(phi0)*np.sin(lam0)*np.sin(lam) + np.sin(phi0)*np.cos(lam)*np.sin(phi)
+#     yr = -np.sin(phi0)*np.cos(lam0)*np.cos(lam)*np.cos(phi) - np.sin(phi0)*np.sin(lam0)*np.sin(lam) + np.cos(phi0)*np.cos(lam)*np.sin(phi)
+#     zr = -np.sin(lam0)*np.cos(lam)*np.cos(phi) + np.cos(lam0)*np.sin(lam)
+
+#     phir = np.arctan2(yr, xr)
+#     lamr = np.arcsin(zr)
+
+#     dxrdp = -np.cos(phi0)*np.cos(lam0)*np.cos(lam)*np.sin(phi) + np.sin(phi0)*np.cos(lam)*np.cos(phi)
+#     dyrdp = np.sin(phi0)*np.cos(lam0)*np.cos(lam)*np.sin(phi) + np.cos(phi0)*np.cos(lam)*np.cos(phi)
+#     dzrdp = np.sin(lam0)*np.cos(lam)*np.sin(phi)
+
+#     dxrdl = -np.cos(phi0)*np.cos(lam0)*np.sin(lam)*np.cos(phi) + np.cos(phi0)*np.sin(lam0)*np.cos(lam) - np.sin(phi0)*np.sin(lam)*np.sin(phi)
+#     dyrdl = np.sin(phi0)*np.cos(lam0)*np.sin(lam)*np.cos(phi) - np.sin(phi0)*np.sin(lam0)*np.cos(lam) - np.cos(phi0)*np.sin(lam)*np.sin(phi)
+#     dzrdl = np.sin(lam0)*np.sin(lam)*np.cos(phi) + np.cos(lam0)*np.cos(lam)
+
+
+#     dprdp = (dyrdp*xr-yr*dxrdp)/(xr**2+yr**2)
+#     dprdl = (dyrdl*xr-yr*dxrdl)/(xr**2+yr**2)
+
+#     dlrdp = dzrdp/np.sqrt(1-zr**2)
+#     dlrdl = dzrdl/np.sqrt(1-zr**2)
+
+#     f1, f2, f3, g1, g2, g3, d1, d2, d3, e1, e2, e3 = A.basevectors_apex(lam*180./np.pi, phi*180./np.pi, 0., coords='apex')
+#     glat, glon, _ = A.apex2geo(lam, phi, 0.)
+#     u, v, w = pm.enu2uvw(d1[0], d1[1], d1[2], glat, glon)
+#     d1 = np.array([u, v, w])
+#     u, v, w = pm.enu2uvw(d2[0], d2[1], d2[2], glat, glon)
+#     d2 = np.array([u, v, w])
+
+#     gradp = d1/np.cos(lam)
+#     gradl = -np.sqrt(4-3*np.cos(lam)**2)*d2/(2*np.sin(lam))
+
+#     gradpr = dprdp*gradp + dprdl*gradl
+#     gradlr = dlrdp*gradp + dlrdl*gradl
+
+#     # d1r = np.cos(lamr)*gradpr
+
+#     # d2r = -2*np.sin(lamr)/np.sqrt(4-3*np.cos(lamr)**2)*gradlr
+
+#     d1r = gradpr
+#     d2r = gradlr
+
+#     # d1r = np.cos(lamr)/(xr**2+yr**2)*((np.cos(lam)*np.cos(lam0)+np.cos(phi)*np.sin(lam0)*np.sin(lam))*d1 + np.sin(lam0)*np.sin(phi)*np.sqrt(4-3*np.cos(lam)**2)/(2*np.sin(lam))*d2)
+
+#     # d2r = -2*np.sin(lamr)/(np.sqrt(4-3*np.cos(lamr)**2)*np.sqrt(1-zr**2))*(np.sin(lam0)*np.sin(phi)*d1 - (np.sin(lam0)*np.sin(lam)*np.cos(phi)+np.cos(lam0)*np.cos(lam))*np.sqrt(4-3*np.cos(lam)**2)/(2*np.sin(lam))*d2)
+#     # print(d1, d2)
+
+#     return d1r, d2r
+
